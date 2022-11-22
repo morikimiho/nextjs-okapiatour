bed30d4094766215e0f3319c4e48d187b346aa24
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2022-11-22 15:14:10 +0900

    微調整をしました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home () { 
   return (
     <div className='topimage'>
-      <Image src='/images/topimage.jpg' alt='絶景の画像' width={1500} height={750}/>
+      {/* <Image src='/images/topimage.jpg' alt='絶景の画像' width={1500} height={750}/> */}
     </div>
   );
 }
