05b7f0bdceecc14fb8fec04a1c976c4118ab038f
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-11-22 14:30:19 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,14 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
+    
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
