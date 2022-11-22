9122f7ed93d862a83533340b74f0f0217d0245e6
Madoka Kato <maka7816@yahoo.co.jp>
2022-11-22 15:13:44 +0900

    modified

---
+++ pages/[id].tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Tripdetai() {
+  <>
+    <Head>
+      <title></title>
+    </Head>
+    <Image />
+    <Image />
+    <Image />
+    <div></div>
+  </>;
+  
+}
