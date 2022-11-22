528225540c94eef837bb79ad6800d66526722954
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2022-11-22 19:48:58 +0900

    ヘッダーを作成しました。

--- component/header.tsx
+++ component/header.tsx
@@ -1,3 +1,17 @@
+import styles from '../styles/header.module.css';
+
 export function Header () {
-    
+    return(
+        <div className={styles.headerall}>
+            <div className={styles.header}>
+                <p className={styles.logo}>
+                    <a href="/trip">Trip</a>
+                </p>
+                <div className={styles.buttons}>
+                    <button className={styles.button}>カート</button>
+                    <button className={styles.button}>ログイン</button>
+                </div>
+            </div>
+        </div>
+    );
 }
--- pages/trip/index.tsx
+++ pages/trip/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/toppage.module.css";
+import {Header} from "../../component/header";
 
 export default function Home () { 
     return (
@@ -8,6 +9,7 @@ export default function Home () {
             <Head>
                 <title>trip</title>
             </Head>
+            <Header />
             <div className={styles.container}>
                 <Image className={styles.top} src="/images/topimage.jpg" alt='絶景の画像' width={1500} height={700}/>
                 <button type="button" className={styles.search}>
