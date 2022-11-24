1d2d3c90cdcde3dee631d8f220c43cf0a715c4fc
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2022-11-24 10:57:01 +0900

    commit

---
+++ pages/booking_confirmation.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function BookingConfirmation(props: any) {
+    return(
+        <>
+        <Head>
+            <title>予約確認フォーム</title>
+        </Head>
+        <main>
+            <h1> {}さん、こんにちは！</h1>
+            <p>現在ご予約いただいている内容の確認が可能です。</p>
+
+        </main>
+        </>
+    );
+}
