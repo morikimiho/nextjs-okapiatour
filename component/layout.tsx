811306f2f085bc466ad2d6bde1d7c4e4eec67207
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-11-24 16:27:04 +0900

    共通レイアウト作成

--- component/layout.tsx
+++ component/layout.tsx
@@ -1,6 +1,35 @@
+import { Header } from "./header";
+import { Footer } from "./footer";
+import styles from "../styles/layout.module.scss";
 
-
-const Layout = () => {
-
-}
+const Layout = ({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) => {
+  return (
+    <>
+    {/* index.tsx に　home入れる */}
+    {home ? (
+        <>
+        <Header />
+        <div className={styles.tour_trip_home}>
+             {children}
+           </div>
+        <Footer />
+        </>
+        ) : !home && (
+         <>
+           <Header />
+           <div className={styles.tour_trip_contents}>
+             {children}
+           </div>
+           <Footer />
+           </>
+        )}
+    </>
+  );
+};
 export default Layout;
