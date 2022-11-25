ed56e0cbfed6a96140cc850b5ed0914c12fbd2f5
Madoka Kato <maka7816@yahoo.co.jp>
2022-11-25 10:49:37 +0900

    ツアー詳細のボタン・時間をコンポーネント化しました。

--- pages/trip/[id].tsx
+++ pages/trip/[id].tsx
@@ -8,6 +8,7 @@ import { TripdetailAttention } from "../../component/tripdetailAttention";
 import { TripdetailActivity } from "../../component/tripdetailActivity";
 import { TripdetailImage } from "../../component/tripdetailImage";
 import Layout from "../../component/layout";
+import { TripdetailTimes } from "../../component/tripdetailTimes";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const trips = await getActiveTrips();
@@ -54,50 +55,8 @@ export default function Tripdetail({ trip }) {
           </div>
           <div className={styles.tripdetail}>
             <div>
-              <div>
-                <TripdetailCount trip={trip} />
-              </div>
-              <div>
-                <p>希望の日付を選択してください</p>
-                <input type="date" />
-              </div>
-              <div>
-                <p>希望の時間帯を選択してください</p>
-
-                <div>
-                  {trip.times >= 7 && (
-                    <select>
-                      <option value="1">9:00</option>
-                      <option value="2">10:00</option>
-                    </select>
-                  )}
-
-                  {trip.times < 7 && trip.times >= 3 && (
-                    <select>
-                      <option value="1">9:00</option>
-                      <option value="2">10:00</option>
-                      <option value="3">11:00</option>
-                      <option value="4">12:00</option>
-                      <option value="5">13:00</option>
-                    </select>
-                  )}
-
-                  {trip.times < 3 && (
-                    <select name="" id="">
-                      <option value="1">9:00</option>
-                      <option value="2">10:00</option>
-                      <option value="3">11:00</option>
-                      <option value="4">12:00</option>
-                      <option value="5">13:00</option>
-                      <option value="6">14:00</option>
-                      <option value="7">15:00</option>
-                      <option value="8">16:00</option>
-                      <option value="9">17:00</option>
-                      <option value="10">18:00</option>
-                    </select>
-                  )}
-                </div>
-              </div>
+              <TripdetailCount trip={trip} />
+              <TripdetailTimes trip={trip} />
             </div>
             <TripdetailAttention />
           </div>
