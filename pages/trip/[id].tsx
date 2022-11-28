482a59188371465c4941e4a8a9b7252a761e75eb
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2022-11-28 14:27:05 +0900

    ログイン中の買い物リスト実装しました。（仮）

--- pages/trip/[id].tsx
+++ pages/trip/[id].tsx
@@ -2,13 +2,9 @@ import Head from "next/head";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { getActiveTrips, getTrip } from "../../service/trip";
 import styles from "../../styles/tripdetail.module.css";
-import { TripdetailContent } from "../../component/tripdetailContent";
-import { TripdetailCount } from "../../component/tripdetailCount";
-import { TripdetailAttention } from "../../component/tripdetailAttention";
-import { TripdetailActivity } from "../../component/tripdetailActivity";
-import { TripdetailImage } from "../../component/tripdetailImage";
+import Image from "next/image";
 import Layout from "../../component/layout";
-import { TripdetailTimes } from "../../component/tripdetailTimes";
+
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const trips = await getActiveTrips();
@@ -42,33 +38,14 @@ export default function Tripdetail({ trip }) {
       </Head>
       <Layout>
         <main className={styles.main}>
-          <p>
-            {trip.area} &nbsp;{trip.country}
-          </p>
-
           <h1>{trip.tourName} </h1>
-          <TripdetailImage trip={trip} />
+          <Image src={trip.img1} alt={"ツアー画像"} width={200} height={100} />
           <p>{trip.description}</p>
-          <div className={styles.tripinfo}>
-            <TripdetailActivity trip={trip} />
-            <TripdetailContent trip={trip} />
-          </div>
-          <div className={styles.tripdetail}>
-            <div>
-              <TripdetailCount trip={trip} />
-              <TripdetailTimes trip={trip} />
-            </div>
-            <TripdetailAttention />
-          </div>
-          <br />
           <br />
           <div>
             合計金額(入れた人数の合計金額を出す？？)
             <br />
             XXXXXX円
-          </div>
-          <div className={styles.buttonposition}>
-            <button className={styles.button}>予約に進む</button>
           </div>
         </main>
       </Layout>
