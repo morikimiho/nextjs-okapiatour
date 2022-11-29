30482e0b917442eb6664141d0a781c1aef283e85
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2022-11-29 14:23:15 +0900

    カート遷移前に情報をJSONサーバーにPOSTできるよう実装しました。

--- pages/tour/[id].tsx
+++ pages/tour/[id].tsx
@@ -9,6 +9,8 @@ import { TripdetailActivity } from "../../component/tripdetailActivity";
 import { TripdetailImage } from "../../component/tripdetailImage";
 import Layout from "../../component/layout";
 import { TripdetailTimes } from "../../component/tripdetailTimes";
+import { useState } from "react";
+import router from "next/router";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const tours = await getActiveTrips();
@@ -34,7 +36,60 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 };
 
-export default function Tripdetail({ tour }) {
+export default function Tripdetail({ tour }:{tour:{
+  img1:string
+  tourName:string
+  description:string
+  price:number
+  times:number
+  area:string
+  country:string
+}}) {
+  const [tourDate, setTourDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState(1);
+
+  async function PostData() {
+    const data = {
+      tourDate: tourDate,//新規データ
+      startTime: startTime, //新規データ
+      img1: tour.img1,
+      tourName: tour.tourName,
+      description: tour.description,
+      numberOfPeople: numberOfPeople,//新規データ
+      price: tour.price,
+      total: tour.price*numberOfPeople
+    }
+    // if(login === true) {
+      await fetch('http://localhost:8000/inCart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+  
+  
+      .then((response) => response.json())
+      .then((data) => {
+          console.log(data);
+          // router.push('http://localhost:3000/tour/cart')
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+      
+    // } else {
+    //   localStorage.setItem(tourDate,tourDate)
+    //   localStorage.setItem(img1, tour.img1),
+    //   localStorage.setItem(tourName, tour.tourName),
+    //   localStorage.setItem(description, tour.description),
+    //   localStorage.setItem(countPeople, countPeople),//新規データ
+    //   localStorage.setItem(price, tour.price)
+    //   localStorage.setItem(total, tour.price*numberOfPeople)
+    // }
+  }
+
   return (
     <>
       <Head>
@@ -55,20 +110,15 @@ export default function Tripdetail({ tour }) {
           </div>
           <div className={styles.tripdetail}>
             <div>
-              <TripdetailCount tour={tour} />
-              <TripdetailTimes tour={tour} />
+              <TripdetailCount setNumberOfPeople={setNumberOfPeople}/>
+              <TripdetailTimes tour={tour} setTourDate={setTourDate} setStartTime={setStartTime} />
             </div>
             <TripdetailAttention />
           </div>
           <br />
           <br />
-          <div>
-            合計金額(入れた人数の合計金額を出す？？)
-            <br />
-            XXXXXX円
-          </div>
           <div className={styles.buttonposition}>
-            <button className={styles.button}>予約に進む</button>
+            <button className={styles.button} onClick={PostData}>カートに入れる</button>
           </div>
         </main>
       </Layout>
