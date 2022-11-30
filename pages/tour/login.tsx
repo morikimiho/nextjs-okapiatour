bf9b03343e76d45b9a0327263ffd341a0f187f38
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-11-30 09:34:28 +0900

    ログイン機能実装 - メアドパス入力　=> データがあったらクッキー付与 - ヘッダーでクッキーからユーザー情報取得 - クッキー削除でログアウト - css一部修正
    ???
    ログアウト時の挙動が少しおかしい
    - ログアウトボタン押しても消えない時がある

--- pages/tour/login.tsx
+++ pages/tour/login.tsx
@@ -1,50 +1,80 @@
-import Head from "next/head";
-import Image from "next/image";
-import {Header} from "../../component/header";
-import {Footer} from "../../component/footer";
+import { Header } from "../../component/header";
+import { Footer } from "../../component/footer";
 import styles from "../../styles/login.module.css";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-export default function Login () {
-    return (
-        <>
-            <Header />
-            <div className={styles.container}>
-                <Table />
+export default function Login() {
+  const router = useRouter();
+  const [mailAddress, setMailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const data = {
+    mailAddress: mailAddress,
+    password: password,
+  };
+
+  //ログイン処理（CookieにsignedIn=trueとする）
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        console.log(response);
+        response.json();
+        if (response.status !== 200) {
+          console.log('失敗')
+        }
+        else if (response.status === 200) {
+          router.push("/tour");
+          console.log('ok')
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h3 className={styles.title}>下記からログインしてください。</h3>
+        <div className={styles.inner_border}>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>メールアドレス</label>
+              <input
+                type="email"
+                name="mailAddress"
+                onChange={(e) => setMailAddress(e.target.value)}
+              />
             </div>
-
-            <Link href='http://localhost:3000/trip/create-user'>
-            <button type="button" className={styles.regibutton}>新規登録はこちら</button>
-            </Link>
-
-            <Footer />
-        </>
-    );
-}
-
-function Table () {
-    return (
-        <>
-            <h3 className={styles.title}>下記からログインしてください。</h3>
-            <div className={styles.innerborder}>
-                <div className={styles.table}>
-                    <table>
-                        <tbody>
-                            <tr className={styles.tr}>
-                                <th>メールアドレス</th>
-                                <td><input type="text"/></td>
-                            </tr>
-                            <tr className={styles.tr}>
-                                <th>パスワード</th>
-                                <td><input type="text"/></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </div>
-                <button type="button" className={styles.button}>
-                    <a href="#">ログイン</a>
-                </button>
+            <div>
+              <label>パスワード</label>
+              <input
+                type="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-        </>
-    )
+            <button className={styles.button}>ログイン</button>
+          </form>
+        </div>
+      </div>{/* container */}
+
+      <button type="button" className={styles.reg_button}>
+        <Link href="create-user">新規登録はこちら</Link>
+      </button>
+      <Footer />
+    </>
+  );
 }
