df1d4254dddf91122633ea358069c6bb95faba58
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-12-02 16:04:50 +0900

    画像リサイズ・css全体的に編集

--- component/tripdetailImage.tsx
+++ component/tripdetailImage.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image"
+import Image from "next/image";
+import styles from "../styles/tripdetail.module.css";
 
-export function TripdetailImage({tour}){
-    return (
+export function TripdetailImage({ tour }) {
+  return (
+    <div className={styles.tour_detail_images}>
+
+      <div>
+        <Image src={tour.img1} alt={tour.tourName} width={586} height={400} />
+      </div>
+
+      <div className={styles.tour_detail_image}>
         <div>
-            <Image src={tour.img1} alt={tour.tourName} width={220} height={150} />
-            <Image src={tour.img2} alt={tour.tourName} width={220} height={150} />
-            <Image src={tour.img3} alt={tour.tourName} width={220} height={150} />
+          <Image src={tour.img2} alt={tour.tourName} width={293} height={200} />
         </div>
-    )
+        <div>
+          <Image src={tour.img3} alt={tour.tourName} width={293} height={200} />
+        </div>
+      </div>
+
+    </div>
+  );
 }
