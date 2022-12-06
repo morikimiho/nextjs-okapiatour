23a60676d40a95849b6cc42d3c05e3563d517a64
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2022-12-06 16:27:56 +0900

    最少人数の表示を変更しました。

--- component/tripdetailActivity.tsx
+++ component/tripdetailActivity.tsx
@@ -8,7 +8,7 @@ export function TripdetailActivity({tour}) {
           <li>無料キャンセル(ご出発日三日目まで)</li>
           <li>今すぐ予約＆後で支払う</li>
           <li>所要時間：{tour.times}時間</li>
-          <li>最小催行人数：{tour.minPeople}人</li>
+          {/* <li>最小催行人数：{tour.minPeople}人</li> */}
           <li>集合場所：{tour.meetingPlace}</li>
           <li>金額：1名様&nbsp;{tour.price}円</li>
         </ul>
