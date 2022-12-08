1655bebc478a947c40cf8229ab04064a67dda6cc
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-12-08 16:19:04 +0900

    aa

--- pages/tour/index.tsx
+++ pages/tour/index.tsx
@@ -83,7 +83,7 @@ export default function Home() {
         <title>Okapia Tour</title>
       </Head>
 
-      {/* トップアニメーション */}
+      {/* トップページアニメーション */}
       {/* <div
         className={styles.logos}
         style={{
