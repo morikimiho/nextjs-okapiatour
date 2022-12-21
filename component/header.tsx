4741c413c4720d573209db95ab48507b9008abe5
Madoka Kato <maka7816@yahoo.co.jp>
2022-12-21 10:35:32 +0900

    imageのエラー解消のためにlegacyを追加記載

--- component/header.tsx
+++ component/header.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/header.module.css";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Router from "next/router";
--- component/serchPage/searchSelect.tsx
+++ component/serchPage/searchSelect.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { Tour } from "../../types/types";
 import styles from "../../styles/search-page.module.css";
 import Link from "next/link";
