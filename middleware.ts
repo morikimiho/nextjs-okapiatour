7b8d79086cf570d538848e62da326878958aa60c
mhatano <masaki.hatano@rakus-partners.co.jp>
2022-12-22 11:13:12 +0900

    matcherページ全部入れた

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,29 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 const config = {
-  matcher:'/tour',
+  matcher: [
+    "/",
+    "/tour/index",
+    "/tour/[id]",
+    "/tour/booking_confirmation",
+    "/tour/booking_done",
+    "/tour/cart",
+    "/tour/create-user",
+    "/tour/login",
+    "/tour/pay",
+    "/tour/comment/[id]",
+    "/tour/map/[areaId]",
+    "/tour/map/search-map",
+  ],
 };
 
 const middleware = (req: NextRequest) => {
-  const authBasic = req.headers.get('authorization');
+  const authBasic = req.headers.get("authorization");
   const url = req.nextUrl;
 
   if (authBasic) {
-    const [_, val] = authBasic.split(' ');
-    const [user, pass] = atob(val).split(':');
+    const [_, val] = authBasic.split(" ");
+    const [user, pass] = atob(val).split(":");
 
     if (
       user === process.env.BASIC_AUTH_USER &&
@@ -20,7 +33,7 @@ const middleware = (req: NextRequest) => {
     }
   }
 
-  url.pathname = '/api/auth-basic';
+  url.pathname = "/api/auth-basic";
 
   return NextResponse.rewrite(url);
 };
