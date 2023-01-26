81bdc89b346c24805fb60c2a30b9727c2aa9cb0d
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2023-01-26 14:00:04 +0900

    データベースをprismaに変更しました。

--- pages/api/supabase.ts
+++ pages/api/supabase.ts
@@ -1,16 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../../utils/supabaseClient';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { supabase } from '../../utils/supabaseClient';
 
+// const getAirportAPI = async (_req: NextApiRequest, res: NextApiResponse) => {
 
-const getAirportAPI = async (_req: NextApiRequest, res: NextApiResponse) => {
+//   const { data, error } = await supabase.from('tours').select("*");
 
-  const { data, error } = await supabase.from('tours').select("*");
+//   // 401 Unauthorized、認証が必要
+//   if (error) return res.status(401).json({ error: error.message });
+//   // 200番台は、処理が成功して正常にレスポンスができている状態
+//   return res.status(200).json(data);
+// };
 
-  // 401 Unauthorized、認証が必要
-  if (error) return res.status(401).json({ error: error.message });
-  // 200番台は、処理が成功して正常にレスポンスができている状態
-  return res.status(200).json(data);
-};
-
-
-export default getAirportAPI;
+// export default getAirportAPI;
